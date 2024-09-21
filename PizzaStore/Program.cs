ad2bf4d87a70230b788cebaa63d1e19b56a26bd5
Frank Perera <frankperera107@gmail.com>
2024-09-21 20:32:54 +0800

    EF Core setup and get api call.

---
+++ PizzaStore/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "PizzaStore API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment()){
+    app.UseSwagger();
+    app.UseSwaggerUI( c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API v1");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.Run();
